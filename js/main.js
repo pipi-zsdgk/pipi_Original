f0efef79fcc35c72d52007d4f89e2009f9a73557
{pipi-zsdgk} <{sklgm122510@gmail.con}>
2022-09-25 21:41:41 +0900

    view

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function($){
+
+    $('.images').slick({  
+        arrows: true,   
+        autoplay: false,  
+        dots: true,  
+        centerMode: false, 
+        centerPadding: '40%', 
+        infinite: true,
+        slidesToShow: 1,
+    })
+
+});
