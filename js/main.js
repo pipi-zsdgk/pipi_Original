c6b08c6bc791112e366bdce05a25af5f92e2cbb3
{pipi-zsdgk} <{sklgm122510@gmail.con}>
2022-09-28 23:13:25 +0900

    レスポンシブ化

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 $(function($){
 
     $('.images').slick({  
-        arrows: true,   
-        autoplay: false,  
+        arrows: true,    
         dots: true,  
         centerMode: false, 
         centerPadding: '40%', 
         infinite: true,
+        autoplay: true,
+        pauseOnHover: true,
+        prevArrow: '<img src="矢印.png" class="slide-arrow prev-arrow">',
+        nextArrow: '<img src="矢印.png" class="slide-arrow next-arrow">',
         slidesToShow: 1,
     })
 
