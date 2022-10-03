e188d637a3475932bb6ecacc74fb57b91255b85c
{pipi-zsdgk} <{sklgm122510@gmail.con}>
2022-10-03 17:38:40 +0900

    pc版

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ $(function($){
 
     $('.images').slick({  
         arrows: true,    
-        dots: true,  
+        dots: false,  
         centerMode: false, 
         centerPadding: '40%', 
         infinite: true,
         autoplay: true,
         pauseOnHover: true,
-        prevArrow: '<img src="矢印.png" class="slide-arrow prev-arrow">',
-        nextArrow: '<img src="矢印.png" class="slide-arrow next-arrow">',
+        prevArrow: '<img src="/images/矢印.png" class="slide-arrow prev-arrow">',
+        nextArrow: '<img src="/images/矢印.png" class="slide-arrow next-arrow">',
         slidesToShow: 1,
         responsive: [{
             breakpoint: 400,
